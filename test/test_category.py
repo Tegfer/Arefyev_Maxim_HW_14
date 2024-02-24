57262c6da93e15a00eb7638411ca80d5cdc2416a
Arefyev Maksim <tegferot@gmail.com>
2024-02-24 15:37:20 +0500

    Finished version

---
+++ test/test_category.py
@@ -0,0 +1,8 @@
+from src.class_stack import Category
+
+
+def test_category():
+    amber = Category("Amber", "crystallized sap", 500)
+    assert amber.name == "Amber"
+    assert amber.description == "crystallized sap"
+    assert amber.ware == 500
