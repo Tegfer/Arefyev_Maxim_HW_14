c34a7d34a17a8adc4aa971d20dfaaf42b67ac5e6
Arefyev Maksim <tegferot@gmail.com>
2024-03-17 20:42:06 +0500

    HW 16 done

--- test/test_product.py
+++ test/test_product.py
@@ -5,5 +5,5 @@ def test_product():
     amber = Product("Amber", "crystallized sap", 500, "5")
     assert amber.name == "Amber"
     assert amber.description == "crystallized sap"
-    assert amber.price == 500
-    assert amber.number_of == "5"
+    assert amber._price == 500
+    assert amber.quantity == "5"
